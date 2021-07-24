abd5ec2e454cb088e7788a8446edd85e23ce4d65
Aditya <lit2020023@iiitl.ac.in>
2021-07-24 14:49:31 +0530

    Day2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  final int days = 30;
+  final String name = "Rogers";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("It has been $days days, $name!"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
